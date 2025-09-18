045dfa23a41ee9f64e64f0f0b8a0e7e00d383663
Kalindu <kalindugayanjith@gmail.com>
2025-09-18 20:02:43 +0530

    git

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/lanka-layout_project/',
+  base: '/lanka_layout_project/',
 
 })
