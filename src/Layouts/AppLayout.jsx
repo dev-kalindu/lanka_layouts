bba60f8813fd68984d7db406646552bce7b49f44
Kalindu Gayanjith <kalindugayanjith@gmail.com>
2025-10-17 17:07:11 +0530

    js

--- src/Layouts/AppLayout.jsx
+++ src/Layouts/AppLayout.jsx
@@ -6,21 +6,21 @@ import axios from "axios";
 import { FaPhoneAlt } from "react-icons/fa";
 
 function index() {
-  useEffect(() => {
-    const cursorDot = document.querySelector("[data-cursor-dot]");
+  // useEffect(() => {
+  //   const cursorDot = document.querySelector("[data-cursor-dot]");
 
-    window.addEventListener("mousemove", function (e) {
-      const posX = e.clientX;
-      const posY = e.clientY;
-      cursorDot.animate(
-        {
-          left: `${posX}px`,
-          top: `${posY}px`,
-        },
-        { duration: 1000, fill: "forwards" }
-      );
-    });
-  });
+  //   window.addEventListener("mousemove", function (e) {
+  //     const posX = e.clientX;
+  //     const posY = e.clientY;
+  //     cursorDot.animate(
+  //       {
+  //         left: `${posX}px`,
+  //         top: `${posY}px`,
+  //       },
+  //       { duration: 1000, fill: "forwards" }
+  //     );
+  //   });
+  // });
   return (
     <div>
       {/* <div className="cursor-dot d-none d-lg-block" data-cursor-dot></div> */}
